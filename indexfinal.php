e05f70f0197c2d15a3c5342ae4bb82d13bf0f988
Diver David <diverdavid@mac-air.home>
2014-12-16 14:06:41 -0500

    cleaned format

--- indexfinal.php
+++ indexfinal.php
@@ -74,102 +74,100 @@
         class program{
         function __construct(){
             $page = 'home';
-                $arg = NULL;
+            $arg = NULL;
 
-                if(isset($_REQUEST['page'])){
-                    $page = $_REQUEST['page'];
-                }
+            if(isset($_REQUEST['page'])){
+                $page = $_REQUEST['page'];
+            }
 
-                if(isset($_REQUEST['arg'])){
-                    $arg = $_REQUEST['arg'];
-                }
+            if(isset($_REQUEST['arg'])){
+                $arg = $_REQUEST['arg'];
+            }
 
-                $page = new $page($arg);
+            $page = new $page($arg);
 
+            }
+
+        }
+
+        abstract class page{
+            public $content;
+
+            function __construct($arg = NULL){
+                    $this->get();
+            }
+
+            function get(){
+            }
+
+            function __destruct(){
+                echo $this->content;
+            }
+        }
+        class home extends page{
+            function get(){
+                $this->content .= '<h3>Queries are sent to a SQL database and return the top 20 answers to the following</h3>';
                 }
 
             }
 
-            abstract class page{
-                public $content;
-
-                function __construct($arg = NULL){
-                        $this->get();
-                }
-
-                function get(){
-                }
-
-                function __destruct(){
-                    echo $this->content;
-                }
-            }
-
-
-            class home extends page{
-                function get(){
-                    $this->content .= '<h3>Queries are sent to a SQL database and return the top 20 answers to the following</h3>';
-                    }
-
-                }
-
-            class q1 extends page{
-                public function get(){
-                    $query = "SELECT c.INSTNM, e.EFY2011 FROM enrollment e INNER JOIN college c on e.UNITID=c.UNITID ORDER BY e.EFY2011 DESC LIMIT 20";
-                    $results = \Lib\Sql::connect($query);
-                    $table = \Lib\Html::TableEnrollment($results);
-                    $this->content .= '<div><h3>Colleges that have the highest enrollment</h3></div>';
-                    $this->content .= $table;
-                    
-                }
-
-
-            }
-            class q2 extends page{
-                public function get(){
-                    $this->content .= '<h3>Colleges with the largest amount of total liabilities</h3>';
-                    $query = "SELECT c.INSTNM, f.LIAB011 FROM college c INNER JOIN finance f ON c.UNITID=f.UNITID ORDER BY f.LIAB011 DESC LIMIT 20";
-                    $results = \Lib\Sql::connect($query);
-                    $table = \Lib\Html::TableBase($results);
-                    $this->content .= $table;
-                    
-                    
-                    
-                }
-            }
-
-            class q3 extends page{
-                public function get(){
-                    $this->content .= '<h3>Colleges with the largest amount of net assets</h3>';
-                    $query = "SELECT c.INSTNM, f.ASSE011 FROM college c INNER JOIN finance f ON c.UNITID=f.UNITID ORDER BY f.ASSE011 DESC LIMIT 20";
-                    $results = \Lib\Sql::connect($query);
-                    $table = \Lib\Html::TableBase($results);
-                    $this->content .= $table;
-                    
-                }
-            }
-
-        class q4 extends page{
+        class q1 extends page{
             public function get(){
-                $this->content .= '<h3>Colleges with the largest amount of net assets per student</h3>';
-                $query = "SELECT c.INSTNM FROM college c INNER JOIN finance f ON f.UNITID=c.UNITID INNER JOIN enrollment e ON c.UNITID=e.UNITID ORDER BY (f.ASSE011/e.EFY2011) DESC LIMIT 20";
+                $query = "SELECT c.INSTNM, e.EFY2011 FROM enrollment e INNER JOIN college c on e.UNITID=c.UNITID ORDER BY e.EFY2011 DESC LIMIT 20";
                 $results = \Lib\Sql::connect($query);
-                $table = \Lib\Html::TableBase($results);
+                $table = \Lib\Html::TableEnrollment($results);
+                $this->content .= '<div><h3>Colleges that have the highest enrollment</h3></div>';
                 $this->content .= $table;
+
+            }
+
+
+        }
+        class q2 extends page{
+            public function get(){
+                $this->content .= '<h3>Colleges with the largest amount of total liabilities</h3>';
+                $query = "SELECT c.INSTNM, f.LIAB011 FROM college c INNER JOIN finance f ON c.UNITID=f.UNITID ORDER BY f.LIAB011 DESC LIMIT 20";
+                $results = \Lib\Sql::connect($query);
+                $table = \Lib\Html::TableLiab($results);
+                $this->content .= $table;
+
+
+
             }
         }
 
-        class q5 extends page{
+        class q3 extends page{
             public function get(){
-                $this->content .= '<h3>Colleges with the largest percentage increase in enrollment between the years of 2011 and 2010</h3>';
-                $query = "SELECT c.INSTNM FROM college c INNER JOIN enrollment e ON e.UNITID=c.UNITID ORDER BY(e.EFY2010 / e.EFY2011) *100 DESC LIMIT 20";
+                $this->content .= '<h3>Colleges with the largest amount of net assets</h3>';
+                $query = "SELECT c.INSTNM, f.ASSE011 FROM college c INNER JOIN finance f ON c.UNITID=f.UNITID ORDER BY f.ASSE011 DESC LIMIT 20";
                 $results = \Lib\Sql::connect($query);
-                $table = \Lib\Html::TableBase($results);
+                $table = \Lib\Html::TableAsse($results);
                 $this->content .= $table;
+
             }
         }
-            
-            ?>
+
+    class q4 extends page{
+        public function get(){
+            $this->content .= '<h3>Colleges with the largest amount of net assets per student</h3>';
+            $query = "SELECT c.INSTNM, e.EFY2011, f.ASSE011 FROM college c INNER JOIN finance f ON f.UNITID=c.UNITID INNER JOIN enrollment e ON c.UNITID=e.UNITID ORDER BY (f.ASSE011/e.EFY2011) DESC LIMIT 20";
+            $results = \Lib\Sql::connect($query);
+            $table = \Lib\Html::TableAsseStudent($results);
+            $this->content .= $table;
+        }
+    }
+
+    class q5 extends page{
+        public function get(){
+            $this->content .= '<h3>Colleges with the largest percent increase in enrollment from 2010-2011</h3>';
+            $query = "SELECT c.INSTNM, e.EFY2011, e.EFY2010 FROM college c INNER JOIN enrollment e ON e.UNITID=c.UNITID ORDER BY(e.EFY2011/ e.EFY2010) *100 DESC LIMIT 20";
+            $results = \Lib\Sql::connect($query);
+            $table = \Lib\Html::TablePercent($results);
+            $this->content .= $table;
+        }
+    }
+
+        ?>
 
             
           <a href="indexfinal.php?page=q1" class="btn btn-success btn-lg btn-block">Colleges that have the highest enrollment</a>
